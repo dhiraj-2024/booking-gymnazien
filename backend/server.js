d264bf5f39266ec3956d664df1bb71e049fecbf1
Nerdion <neel99khalade@gmail.com>
2025-04-11 01:39:13 +0530

    feat: added hostname from anywhere

--- backend/server.js
+++ backend/server.js
@@ -46,6 +46,6 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
